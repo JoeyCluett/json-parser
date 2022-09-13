aabf8c0b694ee86f44ba7ae2c7353bc02f48f8aa
joeycluett <joesunglassescool@gmail.com>
2022-09-13 09:05:52 -0500

    added copyright notices to files

--- json-parser.c
+++ json-parser.c
@@ -1,11 +1,22 @@
+/*
+This file is part of json-parser.
+
+json-parser is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+json-parser is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along 
+with json-parser. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "json-parser.h"
 
-#include <stdio.h>
-
 #define JSONPARSER_MAX_DEPTH 64
-
 #ifdef JSONPARSER_HAS_MALLOC
 
+#include <stdio.h>
 #include <stdlib.h>
 
 JsonNode_t* JsonParser_default_allocate_node(void* const _unused) {
